b51d186208c6bbc140de3320ddbb774eb0500097
Ilya Boiko <boiko.ilya.evg@gmail.com>
2025-02-04 13:14:04 +0500

    Order

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF_CS_007
+{
+    class Order
+    {
+        public int Number;
+
+        public string Description;
+
+        // ... Другие поля
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("...");
+        }
+    }
+}
